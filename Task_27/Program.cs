bb2b4a156ea38e334c9beba1e7415b681894af3c
Андрей <pjanov84@mail.ru>
2022-08-30 15:55:43 +0300

    Added Task_27

--- Task_27/Program.cs
+++ Task_27/Program.cs
@@ -6,7 +6,23 @@
 // 82 -> 10
 // 9012 -> 12
 
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
+int DigitSum(int number) // выдаёт сумму цифр в числе
+{
+    int count = number;
+    int digit = 0;
+    int result = 0;
 
+    for (int i = 0; i < count; i++)
+    {
+        digit = number % 10;
+        result = result + digit;
+        number = number / 10;
+    }
+    return result;
+}
+
+int sum = DigitSum(number);
+Console.WriteLine($"Сумма цифр в числе: {number} -> {sum}");
