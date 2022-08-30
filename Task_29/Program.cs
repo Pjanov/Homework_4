2366ec0545fe2a1a6b80c3f975c213b84d4c25f9
Андрей <pjanov84@mail.ru>
2022-08-30 19:09:06 +0300

    Added Task_29

--- Task_29/Program.cs
+++ Task_29/Program.cs
@@ -5,3 +5,40 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
+void FillArray(int[] collection) // Этот метод заполняет массив случайными числами от 1 до 8
+{
+    int length = collection.Length; // Команда, узнать кол-во элементов в массиве
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10); // в аргумент (collection) рандомно ложатся новые случайные числа от 1 до 10.
+        index++;
+    }
+}
+
+void PrintArray(int[] col) // Этот метод выводит по порядку значения массива в терминал  
+{
+    int count = col.Length;
+    int i = 0;
+    // Console.Write("Массив: ");
+    while (i < count)
+    {
+        Console.Write($" {col[i]}{(i != count - 1 ? "," : " ")}"); // вывод в терминал
+        i++;
+    }
+}
+
+void Task29() // Выводит результат двух методов
+{
+    int length = 8;
+    int[] array = new int[length];
+    FillArray(array);
+    PrintArray(array);
+    Console.Write($"-> [");
+    PrintArray(array);
+    Console.Write($"]");
+}
+
+Task29();
+
+
